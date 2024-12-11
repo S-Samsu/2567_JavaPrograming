fcbe67629dde1b3bd9f5a8674e24f458d40378de
S-Samsu <samsusaeleema9099@gmail.com>
2024-12-11 10:26:39 +0700

    LabSheet03EX

--- LabSheet03/src/Lab302_1.java
+++ LabSheet03/src/Lab302_1.java
@@ -1,9 +1,28 @@
-
+import javax.swing.*;
 public class Lab302_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        String inputEmail;
+		
+        boolean invalid = false;
 
+        
+		while (true) {
+			inputEmail = JOptionPane.showInputDialog("Input your e-mail : ").toLowerCase();
+			while(inputEmail.startsWith("@") || inputEmail.contains(" ")) {
+			      inputEmail = JOptionPane.showInputDialog("Input your e-mail,agian: ");
+			}
+		
+		
+		invalid = inputEmail.endsWith("@gmail.com")||inputEmail.endsWith("@hotmail.com");		
+		if(invalid) {
+			JOptionPane.showMessageDialog(null, "Your e-mail is "+ inputEmail);
+			break;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Your e-mail is not hotmail or gmail dot com");
+			continue;
+		}		
+	   }
 	}
-
 }
