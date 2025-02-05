32c684a601a904f402341598de83477362369084
S-Samsu <samsusaeleema9099@gmail.com>
2025-02-05 11:25:02 +0700

    Practice

---
+++ LabSheet9_InheritanceAndComposition/src/Director.java
@@ -0,0 +1,16 @@
+
+public class Director {
+	private String name;
+	private String email;
+	private char gender;
+	
+	Director(String name,String email,char gender){
+		this.name = name;
+		this.email = email;
+		this.gender = gender;
+	}
+	
+	Director(String name,String email){
+		
+	}
+}
