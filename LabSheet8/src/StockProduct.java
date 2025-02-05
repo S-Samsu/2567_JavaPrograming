4a32c30d046c40624f65ffae8b47389a5f55b8fa
S-Samsu <samsusaeleema9099@gmail.com>
2025-02-05 08:36:15 +0700

    Attendance

--- LabSheet8/src/StockProduct.java
+++ LabSheet8/src/StockProduct.java
@@ -1,9 +1,23 @@
-
+import java.util.*;
 public class StockProduct {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		
+		Product[] productList = new Product[4];
+		for (int i=0;i<=4;i++) {
+		productList[i] = new Product();
+		System.out.print("Input product ID : ");
+		productList[i].setId(input.nextLine());		    	
+		System.out.print("Input product Unit : ");
+		int unit = input.nextInt();
+		input.nextLine();
+		     while(unit<=0) {
+		    	System.out.print("Input product Unit : ");
+			    unit = input.nextInt();
+			    input.nextLine();
+			    productList[i].setUnit(unit);
+		    }
+		}
 	}
-
 }
