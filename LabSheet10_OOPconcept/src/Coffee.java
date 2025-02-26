3f0a45239a8cb97f2b3040c75837443680d4fddc
S-Samsu <samsusaeleema9099@gmail.com>
2025-02-26 08:36:07 +0700

    Attendance

--- LabSheet10_OOPconcept/src/Coffee.java
+++ LabSheet10_OOPconcept/src/Coffee.java
@@ -1,4 +1,13 @@
 
-public class Coffee {
-
+public class Coffee extends Drink{
+	private int typeCoffee;
+	private char sizeCoffee;
+	private Barista barista;
+	
+	Coffee(int type , char sizeCoffee , int typeCoffee , Barista barista) {
+		super(type,sizeCoffee);
+		//this.sizeCoffee = sizeCoffee;
+		this.typeCoffee = typeCoffee;
+		this.barista = barista;
+	}
 }
--- LabSheet10_OOPconcept/src/Drink.java
+++ LabSheet10_OOPconcept/src/Drink.java
@@ -1,4 +1,63 @@
 
 public class Drink {
+	private int type;
+	private char size;
+	
+	Drink(int type, char size){
+		this.type = type;
+		this.size = size;
+	}
+	
+	Drink(){
+		
+	}
 
+	public String getTypeName() {
+		if(this.type == 1) {
+			return "Hot";
+		}if(this.type == 2) {
+			return "Cold";
+		}else {
+			return null;
+		}
+	}
+	
+	public int getTypePrice() {
+		if(this.type == 1) {
+			return 10;
+		}if(this.type == 2) {
+			return 20;
+		}else {
+			return 0;
+		}
+	}
+	
+	public String getSizeName() {
+		if(this.size == 'S') {
+			return "Small";
+		}if(this.size == 'M') {
+			return "Medium";
+		}if(this.size == 'L') {
+			return "Large";
+		}else {
+			return null;
+		}
+	}
+	
+	public int getSizePrice() {
+		if(this.size == 'S') {
+			return 15;
+		}if(this.size == 'M') {
+			return 20;
+		}if(this.size == 'L') {
+			return 25;
+		}else {
+			return 0;
+		}
+	}
+	
+	public int getTotalPrice() {
+		return getSizePrice()+getTypePrice();
+	}
+	
 }
